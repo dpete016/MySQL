767c05016359d3ee573e8e2d920f59ff81350313
Dan <dpete016@gmail.com>
2019-03-07 21:28:42 -0500

    in progress

---
+++ bamazon.sql
@@ -0,0 +1,10 @@
+insert into products(ProductName,DepartmentName,Price,StockQuantity)
+values ("Devil May Cry 5","VideoGames",59.99,200),
+("Left Behind","VideoGames",59.99,200),
+("Kingdom Hearts 3","VideoGames",59.99,200),
+("Bootcut Jeans","Clothing",29.99,75),
+("Cards against Humanity","Board Games",9.99,100),
+("Spider-Man Into the Spider-Verse","Film",19.99,150),
+("Got Whey T-shirt","Clothing",9.99,50);
+
+Select * From bamazon.products;
